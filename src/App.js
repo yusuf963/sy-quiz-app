dbaa8429e5633159aeb379ed92f1e49bd755430d
yusuf963 <mohammad963yusuf@gmail.com>
2021-02-04 02:21:36 +0000

    importing compoennets into app component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import Nav from './Component/Nav'
+import Quiz from './Component/Quiz'
+
 
 import './styles/style.scss'
 
 const App = () => (
   <BrowserRouter>
+    <Nav />
     <Switch>
-      <Route exact path="/project-2" component={Home}/>
+      <Route exact path="/" component={() => <HomePage />} />
+      <Route path="/home" component={() => <HomePage />} />
+      <Route path="/quiz" component={Quiz} />
+      <Route path='/notfound' component={() => <NotFound />} />
+      <Redirect exact from='/' to='/home' />
+      <Redirect to='/notfound' component={() => <NotFound />} />
     </Switch>
   </BrowserRouter>
+
 )
 
-const Home = () => {
+const HomePage = () => {
   return <h1>Hello world</h1>
 }
 
